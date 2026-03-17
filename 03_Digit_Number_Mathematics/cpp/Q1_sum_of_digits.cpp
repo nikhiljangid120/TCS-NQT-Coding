3deba565852003235791b6d288aabfcf17cb45bc
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-17 14:42:23 +0530

    feat: Add C++ solution for the "Sum Of Digits" problem.

--- 03_Digit_Number_Mathematics/cpp/Q1_sum_of_digits.cpp
+++ 03_Digit_Number_Mathematics/cpp/Q1_sum_of_digits.cpp
@@ -8,18 +8,18 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-        int n;
-        cin>>n;
-        int sum=0;
-        while(n>0){
-            sum+=n%10;
-            n/=10;
-        }
-        cout<<sum<<endl;
+    int n;
+    cin>>n;
+    int sum=0;
+    while(n>0){
+        sum+=n%10;
+        n/=10;
+    }
+    cout<<sum<<endl;
     return 0;
 }
