4772b62355b0caa7249833690897da2e771474af
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-17 14:52:49 +0530

    To reverse word order in a string using C++

--- 02_String_Processing/cpp/Q1_reverse_words.cpp
+++ 02_String_Processing/cpp/Q1_reverse_words.cpp
@@ -13,9 +13,26 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    string s;
-    getline(cin,s);
-    reverse(s.begin(),s.end());
-    cout<<s<<endl;
+    // string s;
+    // getline(cin,s);
+    // reverse(s.begin(),s.end());
+    // cout<<s<<endl;
+
+    string s, word, result;
+    getline(cin, s);
+    stringstream ss(s);
+    stack<string> st;
+    
+    while (ss >> word) {
+        st.push(word);
+    }
+    
+    while (!st.empty()) {
+        if (!result.empty()) result += " ";
+        result += st.top(); st.pop();
+    }
+    
+    cout << result << endl;
+
     return 0;
 }
