172906d2c1f5e7e973fb58f4b5d64600589beced
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-17 14:46:32 +0530

    This approach reverses all characters globally.

--- 02_String_Processing/cpp/Q1_reverse_words.cpp
+++ 02_String_Processing/cpp/Q1_reverse_words.cpp
@@ -6,3 +6,16 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    string s;
+    getline(cin,s);
+    reverse(s.begin(),s.end());
+    cout<<s<<endl;
+    return 0;
+}
