abda8edb0991ad82d4c059afa809bedb266cc570
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:47:31 +0530

    kth largest problem done

--- 05_Sorting/cpp/Q10_kth_largest.cpp
+++ 05_Sorting/cpp/Q10_kth_largest.cpp
@@ -6,3 +6,29 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    long long k;
+    cin>>k;
+    // sort(arr.begin(),arr.end());
+    // cout<<arr[n-k]<<"\n";
+    priority_queue<int,vector<int>,greater<int>>pq;
+    for(int i = 0;i<n;i++){
+        pq.push(arr[i]);
+        if(pq.size()>k) pq.pop();
+    }
+    cout<<pq.top()<<"\n";
+    return 0;
+}
