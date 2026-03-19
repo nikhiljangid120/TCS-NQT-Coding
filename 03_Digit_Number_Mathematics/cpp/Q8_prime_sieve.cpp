e26410bf5efde14f547abf8f06a011c399428aa2
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:07:11 +0530

    Prime checker program done

--- 03_Digit_Number_Mathematics/cpp/Q8_prime_sieve.cpp
+++ 03_Digit_Number_Mathematics/cpp/Q8_prime_sieve.cpp
@@ -6,3 +6,22 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    for(int i = 2;i<sqrt(n);i++){
+        if(n%i == 0){
+            cout<<"Not a Prime";
+            return 0;
+        }
+    }
+    cout<<"Prime";
+    return 0;
+}
