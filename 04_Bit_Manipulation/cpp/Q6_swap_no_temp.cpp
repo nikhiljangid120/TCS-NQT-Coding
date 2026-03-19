a8dd1ff12d77c6cd348728ed661acb97e826dab8
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:40:27 +0530

    Swap done without using anything and just using the bitwise manipulations

--- 04_Bit_Manipulation/cpp/Q6_swap_no_temp.cpp
+++ 04_Bit_Manipulation/cpp/Q6_swap_no_temp.cpp
@@ -5,4 +5,21 @@
  * Write a C++ program to solve the "Swap No Temp" problem.
  */
 
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    long long m;
+    cin>>m;
+    m^=n;
+    n^=m;
+    m^=n;
+    cout<<n<<" "<<m<<"\n";
+    return 0;
+}
