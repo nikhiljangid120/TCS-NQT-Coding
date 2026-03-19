0bb3cb247fd1b847a94ae1c1977c7754202934a1
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:29:52 +0530

    Even Odd done Program

--- 04_Bit_Manipulation/cpp/Q1_odd_even.cpp
+++ 04_Bit_Manipulation/cpp/Q1_odd_even.cpp
@@ -6,3 +6,17 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    if(n&1) cout<<"Odd Number";
+    else cout<<"Even Number";
+    return 0;
+}
