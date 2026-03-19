eac5197fd67fbb736bd175128af5aa306159a983
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:36:15 +0530

    XOR Unique done

--- 04_Bit_Manipulation/cpp/Q2_xor_unique.cpp
+++ 04_Bit_Manipulation/cpp/Q2_xor_unique.cpp
@@ -6,3 +6,22 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+    }
+    int xor_sum = 0;
+    for(auto &x:arr) xor_sum^=x;
+    cout<<xor_sum<<"\n";
+    return 0;
+}
