4dbbfcdb1063b1dd363a136f4a186014528a5204
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 15:01:56 +0530

    Factprial Program Done

--- 07_Factorial_and_Combinatorics/cpp/Q1_factorial.cpp
+++ 07_Factorial_and_Combinatorics/cpp/Q1_factorial.cpp
@@ -6,3 +6,19 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    long long fact = 1;
+    for(long long i = 1; i <= n; i++){
+        fact = fact * i;
+    }
+    cout<<fact<<endl;
+}
