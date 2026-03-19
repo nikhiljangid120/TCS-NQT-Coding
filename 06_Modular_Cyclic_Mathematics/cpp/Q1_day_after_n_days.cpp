40a1afbd964a7d6fcd416fbe54ec8e20806d75b2
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:54:49 +0530

    C++ program to solve the "Day After N Days" problem.

--- 06_Modular_Cyclic_Mathematics/cpp/Q1_day_after_n_days.cpp
+++ 06_Modular_Cyclic_Mathematics/cpp/Q1_day_after_n_days.cpp
@@ -6,3 +6,19 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    string days[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    int today = 1;
+    int res = ((today+n)%7);
+    cout<<days[res]<<endl;
+    
+}
