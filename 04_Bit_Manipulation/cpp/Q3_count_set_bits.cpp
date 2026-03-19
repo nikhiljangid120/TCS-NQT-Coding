a80f9554c3bbc8d75cc8eb192efff8d996f3f42e
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:37:42 +0530

    C++ program to solve the "Count Set Bits" problem.

--- 04_Bit_Manipulation/cpp/Q3_count_set_bits.cpp
+++ 04_Bit_Manipulation/cpp/Q3_count_set_bits.cpp
@@ -6,3 +6,17 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    int count = __builtin_popcount(n);
+    cout<<count<<"\n";
+    return 0;
+}
