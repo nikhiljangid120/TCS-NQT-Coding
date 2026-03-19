075b22bb5e6b248528c0f2c4195708b2894ebbe6
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:59:15 +0530

    Updated after n days code logic

--- 06_Modular_Cyclic_Mathematics/cpp/Q1_day_after_n_days.cpp
+++ 06_Modular_Cyclic_Mathematics/cpp/Q1_day_after_n_days.cpp
@@ -6,19 +6,32 @@
  */
 
 
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int main(){
+int main() {
+    // Optimize I/O operations
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    cin.tie(NULL);
 
     long long n;
-    cin>>n;
-    string days[] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-    int today = 1;
-    int res = ((today+n)%7);
-    cout<<days[res]<<endl;
+    if (!(cin >> n)) return 0;
+
+    // Array of days starting from Sunday (index 0)
+    string days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     
+    // Starting point: if 1 is Monday, the logic is correct.
+    // We use (today + n) % 7 to find the index of the future day.
+    int today = 1; 
+    int res = (today + n) % 7;
+
+    // Handle negative n (if the problem allows looking into the past)
+    if (res < 0) res += 7;
+
+    cout << days[res] << endl;
+
+    return 0;
 }
