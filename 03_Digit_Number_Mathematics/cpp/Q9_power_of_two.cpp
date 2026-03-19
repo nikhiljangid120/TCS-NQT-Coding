c0bcbe8baa3e6767769f6670089cb638a23ffb83
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 14:22:32 +0530

    Power of two done

--- 03_Digit_Number_Mathematics/cpp/Q9_power_of_two.cpp
+++ 03_Digit_Number_Mathematics/cpp/Q9_power_of_two.cpp
@@ -6,3 +6,16 @@
  */
 
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    long long n;
+    cin>>n;
+    cout<<pow(2,n)<<"\n";
+    return 0;
+}
