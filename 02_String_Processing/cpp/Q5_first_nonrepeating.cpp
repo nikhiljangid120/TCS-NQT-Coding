e9aa3896aeb22d3652ed85455af9b66c6fc35e7f
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 13:02:23 +0530

    C++ program to solve the "First Nonrepeating" problem.

--- 02_String_Processing/cpp/Q5_first_nonrepeating.cpp
+++ 02_String_Processing/cpp/Q5_first_nonrepeating.cpp
@@ -5,4 +5,25 @@
  * Write a C++ program to solve the "First Nonrepeating" problem.
  */
 
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    string s;
+    getline(cin,s);
+    
+    unordered_set<char>st;
+    for(auto &c:s) st.insert(c);
+    for(auto&c:s){
+        if(st.count(c)==1){
+            cout<<c<<endl;
+            return 0;
+        }
+    }
+    cout<<"No nonrepeating character found"<<endl;
+    return 0;
+}
