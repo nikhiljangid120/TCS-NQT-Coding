710751190724530fba09c98f4ce17fc2b2dfb78a
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 12:57:55 +0530

    C++ program to solve the "Count Vowels" problem.

--- 02_String_Processing/cpp/Q4_count_vowels.cpp
+++ 02_String_Processing/cpp/Q4_count_vowels.cpp
@@ -12,5 +12,13 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    
+    string s;
+    getline(cin,s);
+    string str = "aeiouAEIOU";
+    int count = 0;
+    for(auto c:s){
+        if(str.find(c)!=string::npos) count++;
+    }
+    cout<<count<<endl;
+    return 0;
 }
