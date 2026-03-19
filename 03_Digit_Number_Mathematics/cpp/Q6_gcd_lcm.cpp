369246507ba6c054d63382767e56ff50fd4aca9c
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 13:38:30 +0530

    HCF LCM Done

--- 03_Digit_Number_Mathematics/cpp/Q6_gcd_lcm.cpp
+++ 03_Digit_Number_Mathematics/cpp/Q6_gcd_lcm.cpp
@@ -5,4 +5,20 @@
  * Write a C++ program to solve the "Gcd Lcm" problem.
  */
 
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int a, b;
+    cin>>a>>b;
+
+    int hcf = 0, lcm = 0;
+    hcf = __gcd(a,b);
+    lcm = (a/hcf)*b;
+    cout<<hcf<<" "<<lcm<<endl;
+    return 0;
+}
