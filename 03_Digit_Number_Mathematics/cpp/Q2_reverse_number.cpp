8ac9b878145ac1121f4984c1db30efe617fc3d40
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 13:43:46 +0530

    Reverse Number done

--- 03_Digit_Number_Mathematics/cpp/Q2_reverse_number.cpp
+++ 03_Digit_Number_Mathematics/cpp/Q2_reverse_number.cpp
@@ -5,4 +5,17 @@
  * Write a C++ program to solve the "Reverse Number" problem.
  */
 
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin>>n;
+    string str = to_string(n);
+    reverse(str.begin(),str.end());
+    cout<<str<<endl;
+    return 0;
+}
