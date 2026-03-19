156e9aa2015e006d00013af06499baee48c9ce9c
nikhiljangid120 <nikhilverse58@gmail.com>
2026-03-19 10:18:38 +0530

    feat: Add C++ files for palindrome, anagram, and count vowels problems, including a complete solution for palindrome.

--- 02_String_Processing/cpp/Q2_palindrome.cpp
+++ 02_String_Processing/cpp/Q2_palindrome.cpp
@@ -5,4 +5,19 @@
  * Write a C++ program to solve the "Palindrome" problem.
  */
 
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string s;
+    getline(cin,s);
+    string reversed = s;
+    reverse(reversed.begin(),reversed.end());
+    if(s==reversed) cout<<"Palindrome String!";
+    else cout<<"Not a Palindrome String!";
+    return 0;
+}    
--- 02_String_Processing/cpp/Q3_anagram.cpp
+++ 02_String_Processing/cpp/Q3_anagram.cpp
@@ -5,4 +5,12 @@
  * Write a C++ program to solve the "Anagram" problem.
  */
 
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+}
